abf19323873db77c0c148afdfda959d777fb690f
Adeleke Theophilus <theophilusadesola002@gmail.com>
2022-09-29 15:20:45 +0100

    styling ongoing..

--- index.js
+++ index.js
@@ -26,32 +26,33 @@ searchBtn.addEventListener("click", () => {
                 let returnedWordArray = data
                 for (let wordBank = 0; wordBank < returnedWordArray.length; wordBank++) {
                     phoneticDiv.innerHTML += `
-                    <div><h1 class="blue-text lighten-1">${returnedWordArray[wordBank].word}<span>${wordBank + 1}</span></h1></div>
+                    <div class ="myclass"><h1 class="blue-text lighten-1">${returnedWordArray[wordBank].word}<span><em>${wordBank + 1}</em></span></h1></div>
                     `
                     if (returnedWordArray[wordBank].phonetic != true) {
                         phoneticDiv.innerHTML += ""
                     } else if (returnedWordArray[wordBank].phonetic == true) {
 
                     } {
-                        phoneticDiv.innerHTML += `<div><h1>${returnedWordArray[wordBank].phonetic}</h1></div>`
+                        phoneticDiv.innerHTML += `<div class = "myclass"><p>${returnedWordArray[wordBank].phonetic}</p></div>`
                     }
                     let meaningsArray = returnedWordArray[wordBank].meanings
 
                     for (let meaning = 0; meaning < meaningsArray.length; meaning++) {
                         phoneticDiv.innerHTML += `
-                        <div><h3>Part of speech: ${returnedWordArray[wordBank].meanings[meaning].partOfSpeech}</h3></div>
+                        <div><h3 class = "flow-text">Part of speech: ${returnedWordArray[wordBank].meanings[meaning].partOfSpeech}</h3></div>
                         `
                         let definitionsArray = returnedWordArray[wordBank].meanings[meaning].definitions
+                        phoneticDiv.innerHTML += `<h4><em>definitions</em></h4>`
                         for (let definition = 0; definition < definitionsArray.length; definition++) {
                             phoneticDiv.innerHTML += `
-                            <div><em>definition</em>: ${definitionsArray[definition].definition}</div>
+                            <div><em>${definition + 1}.</em><p>${definitionsArray[definition].definition}</p></div><br>
                             `
                         }
                         let synonymsArray = returnedWordArray[wordBank].meanings[meaning].synonyms
                         if (synonymsArray == []) {
                             phoneticDiv.innerHTML += ""
                         } else {
-                            phoneticDiv.innerHTML += `<h4>Synonyms</h4>`
+                            phoneticDiv.innerHTML += `<h4><em>Synonyms</em></h4>`
                             for (let synonym = 0; synonym < synonymsArray.length; synonym++) {
                                 phoneticDiv.innerHTML += `<div>${synonymsArray[synonym]}</div>
                                 `
