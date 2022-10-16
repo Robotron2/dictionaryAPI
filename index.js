663dda813d307858181eec8c056926055f1998ed
Theophilus <theophilusadesola002@gmail.com>
2022-10-16 20:53:34 +0100

    Random word search function takes random words from another API and makes the auto search

--- index.js
+++ index.js
@@ -4,11 +4,15 @@ let searchBtn = document.getElementById("search");
 
 let shuffleBtn = document.getElementById("shuffle");
 
-// let wordDiv = document.getElementById("wordDiv");
+let errorDiv = document.getElementById("errorDiv");
 
 let phoneticDiv = document.getElementById("phoneticDiv");
 
-console.log(wordList.length);
+// console.log(wordList.length);
+
+const clearError = () => {
+	errorDiv.innerHTML;
+};
 
 searchBtn.addEventListener("click", () => {
 	phoneticDiv.innerHTML = "";
@@ -20,6 +24,7 @@ searchBtn.addEventListener("click", () => {
 	// console.log(searchUrl)
 	if (userInput.value == "") {
 		console.log("Field cannot be empty");
+		errorDiv.innerHTML = `<p>Field cannot be empty</p>`;
 	} else {
 		fetch(searchUrl)
 			.then((response) => {
@@ -103,83 +108,71 @@ shuffleBtn.addEventListener("click", () => {
 	phoneticDiv.innerHTML = "";
 	let searchUrl = `https://api.dictionaryapi.dev/api/v2/entries/en/${randomWord}`;
 
-	// console.log(searchUrl)
-	if (userInput.value == "") {
-		console.log("Field cannot be empty");
-	} else {
-		fetch(searchUrl)
-			.then((response) => {
-				if (response.ok) {
+	// let randomWordApi =
+	fetch("https://random-word-api.herokuapp.com/word")
+		.then((response) => {
+			return response.json();
+		})
+
+		.then((data) => {
+			let randomWordApi = data[0];
+		})
+		.then(
+			fetch(searchUrl)
+				.then((response) => {
 					return response.json();
-				} else {
-					phoneticDiv.innerHTML = "Enter English words only";
-				}
-			})
-			// str.charAt(0).toUpperCase() + str.slice(1)
-			.then((data) => {
-				// console.log(data)
-
-				let returnedWordArray = data;
-
-				for (let wordBank = 0; wordBank < returnedWordArray.length; wordBank++) {
-					phoneticDiv.innerHTML += `
-                    
-                    <div class ="myclass"><h3 class="blue-text lighten-1">${returnedWordArray[wordBank].word.charAt(0).toUpperCase() + returnedWordArray[wordBank].word.slice(1)}<span><em>${
-						wordBank + 1
-					}</em></span></h3></div>
-                    
+				})
+				.then((data) => {
+					console.log(data);
+					let randomWordData = data;
+					for (let wordBank = 0; wordBank < randomWordData.length; wordBank++) {
+						phoneticDiv.innerHTML += `
+                    <div class ="myclass"><h3 class="blue-text lighten-1">${randomWordData[wordBank].word}<span><em>${wordBank + 1}</em></span></h3></div>
                     `;
 
-					let pathToPhonetics = returnedWordArray[wordBank].phonetics[0].text;
-
-					if (pathToPhonetics == undefined) {
-						pathToPhonetics = returnedWordArray[wordBank].phonetics[1].text;
-					}
-
-					phoneticDiv.innerHTML += `<div class = "myclass"><p>${pathToPhonetics}</p></div>`;
-
-					let meaningsArray = returnedWordArray[wordBank].meanings;
-
-					for (let meaning = 0; meaning < meaningsArray.length; meaning++) {
-						phoneticDiv.innerHTML += `
+						let pathToPhonetics = randomWordData[wordBank].phonetics[0].text;
+						if (pathToPhonetics == undefined) {
+							pathToPhonetics = randomWordData[wordBank].phonetics[1].text;
+						}
+						phoneticDiv.innerHTML += `<div class = "myclass"><p>${pathToPhonetics}</p></div>`;
+						let meaningsArray = randomWordData[wordBank].meanings;
+						for (let meaning = 0; meaning < meaningsArray.length; meaning++) {
+							phoneticDiv.innerHTML += `
                         
-                        <div><h3 class = "flow-text">Part of speech: ${returnedWordArray[wordBank].meanings[meaning].partOfSpeech}</h3></div>
+                        <div><h3 class = "flow-text">Part of speech: ${randomWordData[wordBank].meanings[meaning].partOfSpeech}</h3></div>
                         
                         `;
 
-						let definitionsArray = returnedWordArray[wordBank].meanings[meaning].definitions;
+							let definitionsArray = randomWordData[wordBank].meanings[meaning].definitions;
 
-						phoneticDiv.innerHTML += `<h4><em>definitions</em></h4>`;
+							phoneticDiv.innerHTML += `<h4><em>definitions</em></h4>`;
 
-						for (let definition = 0; definition < definitionsArray.length; definition++) {
-							phoneticDiv.innerHTML += `
+							for (let definition = 0; definition < definitionsArray.length; definition++) {
+								phoneticDiv.innerHTML += `
                             
                             <div><em>${definition + 1}.</em><h6>${definitionsArray[definition].definition}</h6></div><br>
                             
                             `;
-						}
+							}
 
-						let synonymsArray = returnedWordArray[wordBank].meanings[meaning].synonyms;
+							let synonymsArray = randomWordData[wordBank].meanings[meaning].synonyms;
 
-						if (synonymsArray == []) {
-							phoneticDiv.innerHTML += "";
-						} else {
-							phoneticDiv.innerHTML += `<h4><em>Synonyms</em></h4>`;
+							if (synonymsArray == []) {
+								phoneticDiv.innerHTML += "";
+							} else {
+								phoneticDiv.innerHTML += `<h4><em>Synonyms</em></h4>`;
 
-							for (let synonym = 0; synonym < synonymsArray.length; synonym++) {
-								phoneticDiv.innerHTML += `<div>${synonymsArray[synonym]}</div>
+								for (let synonym = 0; synonym < synonymsArray.length; synonym++) {
+									phoneticDiv.innerHTML += `<div>${synonymsArray[synonym]}</div>
                                 
                                 `;
+								}
 							}
 						}
 					}
-				}
-			});
-	}
-	userInput.value = "";
+				})
+		);
 });
-
-//Navbar
 
 // fetch the URL, find the sound, play the sound
 
@@ -189,7 +182,7 @@ shuffleBtn.addEventListener("click", () => {
 	}); // end of document ready
 })(jQuery); // end of jQuery name space
 
-//Preloader
+// Preloader;
 
 const preloader = document.querySelector("#preloader");
 if (preloader) {
